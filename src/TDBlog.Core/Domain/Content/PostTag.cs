761abf030b0039fa9ea4d40d9b50aa168a57db9b
QuyLD <quyld@vnext.vn>
2024-03-01 19:15:24 +0700

    add itentity #1

--- src/TDBlog.Core/Domain/Content/PostTag.cs
+++ src/TDBlog.Core/Domain/Content/PostTag.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TDBlog.Core.Domain.Content
 {
-    internal class Posttag
+    [Table("Posttags")]
+    [PrimaryKey(nameof(PostId), nameof(TagId))]
+    public class PostTag
     {
+        public Guid PostId { get; set; }
+        public Guid TagId { get; set; }
     }
 }
